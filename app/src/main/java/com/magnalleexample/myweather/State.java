1cce7d6ef56ac5167ea687d3370d323ea0cb35c1
Valentina <yasu@list.ru>
2020-12-31 22:03:53 +0700

    Lesson 3

---
+++ app/src/main/java/com/magnalleexample/myweather/State.java
@@ -0,0 +1,19 @@
+package com.magnalleexample.myweather;
+
+public class State {
+    private int number;
+
+    public int getNumber() {
+        return number;
+    }
+
+    static State data;
+    private State(){}
+    public static State getInstance(){
+        if(data == null) {
+            data = new State();
+            data.number = (int)(Math.random() * 100);
+        }
+        return data;
+    }
+}
